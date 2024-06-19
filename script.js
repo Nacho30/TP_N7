96afd7e813176d7aedbda06525b7a06fa3333aaf
Nacho30 <ignaciomartinrodriguez23@gmail.com>
2024-06-19 20:06:42 -0300

    TP 7 TERMINADO

---
+++ script.js
@@ -0,0 +1,67 @@
+// 1)let nombre = "Ignacio Rodriguez";
+// console.log(nombre);
+// Ignacio Rodriguez
+
+// 2)let respuesta = prompt("Ingresa tu edad");
+// alert("Tu edad es.. " + respuesta);
+// Tu edad es.. 19
+
+// 3)let num1= 2;
+// let num2= 6;
+// respuesta= num1+num2;
+// alert(respuesta);
+// 8
+
+// 4)let texto1= "HOLA SOY ";
+// let texto2= "IGNACIO Y ESTOY USANDO JAVASCRIPT";
+// respuesta= texto1+texto2;
+// console.log(respuesta);
+// HOLA SOY IGNACIO Y ESTOY USANDO JAVASCRIPT
+
+// 5)let myarray= [25,true,"JERE","IGNACIO EL MAS CRACK",19,false];
+// alert(myarray[3]);
+// IGNACIO EL MAS CRACK
+
+// 6)let num = 10; // Puedes asignarle cualquier número
+// num += 5; // Incrementa el valor de num en 5
+// console.log(num); // Muestra el resultado en la consola
+// 15
+
+// 7)let cadena= "EL NACHO SIEMPRE SE PONE LA ";
+// cadena+=10;
+// console.log(cadena);
+// EL NACHO SIEMPRE SE PONE LA 10
+
+// 8)let frutas= ["manzana","banana","ananá","pera","melon"];
+// alert(frutas.length);
+// 5
+
+// 9)let texto= "ignacio "
+// texto+= "rodriguez"
+// console.log(texto)
+// ignacio rodriguez
+
+// 10)let num1=8;
+// let num2=9;
+// respuesta= num1*num2;
+// alert(respuesta)
+// 72
+
+// 11)let arreglo= [];
+// arreglo.push("IGNACIO","THE WHITE TIGER",false);
+// console.log(arreglo);
+// IGNACIO,THE WHITE TIGER,false
+
+// 12)let texto= "IGNACIO MARTIN RODRIGUEZ";
+// alert(texto.length);
+// 24
+
+// 13)let num1= 20
+// respuesta= (20*10)-5
+// console.log(respuesta)
+// 195
+
+// 14)let arreglo=["IGNACIO","JERE",22,true];
+// resultado= arreglo.join(",");
+// console.log(resultado);
+//IGNACIO,JERE,22,true
